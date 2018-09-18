5a6a24a519b21f2e693250a0ff7b0cc850f0e858
Nathalie Santana <nathalie.scruz@gmail.com>
2018-09-18 03:44:53 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -27,6 +27,17 @@ class Student
 
   end
 
+  def self.all_students_in_grade_9
+    sql = <<-SQL
+      SELECT *
+      FROM students
+      WHERE grade =< ?
+    SQL
+
+    DB[:conn].execute(sql, 11)
+
+  end
+
   def self.find_by_name(name)
     sql = <<-SQL
       SELECT *
