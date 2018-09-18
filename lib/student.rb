6db16625701d6c178d3b51a0446ad12fe0c37dab
Nathalie Santana <nathalie.scruz@gmail.com>
2018-09-18 04:11:06 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -95,10 +95,10 @@ class Student
     sql = <<-SQL
       SELECT *
       FROM students
-      WHERE grade = ?
+      WHERE grade = 10
       LIMIT 1
     SQL
-    DB[:conn].execute(sql, 10).map do |row|
+    DB[:conn].execute(sql).map do |row|
       self.new_from_db(row)
     end
 
