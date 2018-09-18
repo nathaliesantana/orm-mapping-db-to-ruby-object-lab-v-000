0037d968b8864ca33936235a0a9624d7b63449c1
Nathalie Santana <nathalie.scruz@gmail.com>
2018-09-18 03:45:34 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -27,11 +27,11 @@ class Student
 
   end
 
-  def self.all_students_in_grade_9
+  def self.students_below_12th_grade
     sql = <<-SQL
       SELECT *
       FROM students
-      WHERE grade =< ?
+      WHERE grade <= ?
     SQL
 
     DB[:conn].execute(sql, 11)
