7d4affc004f53a80ec0c4549d6b8bdb5c202789c
Nathalie Santana <nathalie.scruz@gmail.com>
2018-09-18 04:12:15 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -101,8 +101,15 @@ class Student
     DB[:conn].execute(sql).map do |row|
       self.new_from_db(row)
     end.first
+  end
 
-
+  def self.all_students_in_grade_X(x)
+    sql = <<-SQL
+      SELECT *
+      FROM students
+      WHERE grade = ?
+    SQL
+    DB[:conn].execute(sql, x)
   end
 
 
